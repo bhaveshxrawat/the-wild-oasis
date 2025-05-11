cb3a29251bd715b3a781c58f130a84e1ae82b4e0
Bhavesh Rawat <rawatbhavesh2001@gmail.com>
2025-05-11 23:32:01 +0530

    fixed the stale cache bug

--- src/features/authentication/hooks/useLogin.ts
+++ src/features/authentication/hooks/useLogin.ts
@@ -1,15 +1,16 @@
 import { login } from "@/services/apiAuth";
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import toast from "react-hot-toast";
 import { useNavigate } from "react-router-dom";
 
 export function useLogin() {
+  const queryClient = useQueryClient();
   const navigate = useNavigate();
   const { mutate, isPending } = useMutation({
     mutationFn: ({ email, password }: { email: string; password: string }) =>
       login({ email, password }),
     onSuccess: (data) => {
-      console.log(data);
+      queryClient.setQueryData(["user"], data.user);
       navigate("/dashboard", { replace: true });
     },
     onError: (err) => {
